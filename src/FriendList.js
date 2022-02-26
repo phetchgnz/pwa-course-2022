1581dee421fdc890b1abcf22df59a36b7015bea8
phetchgnz <phetchgnz@gmail.com>
2022-02-26 16:59:26 +0700

    ex07-1

--- src/FriendList.js
+++ src/FriendList.js
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import './FriendList.css';
 import App from './components/App';
 import Avatar from './components/Avatar';
+import Userlist from './components/Userlist';
+import Chatbox from './components/Chatbox';
 
 function FriendList(){
   let [friends, setFriends] = useState([
-    {user: "user1", displayName: "ไอ้ปื้ด"},
+    {user: "user1", displayName: "ไอ้ปื้ด", },
     {user: "user2", displayName: "นางสาวสุดา"},
     {user: "user3", displayName: "พลทหารหนึ่ง"},
     {user: "user4", displayName: "ศาตราจารย์นายกรัฐมนตรีดอกเตอร์เขียว"},
@@ -13,8 +15,15 @@ function FriendList(){
 
   return(
     <App>
-      <div className="friend-list">
-       ใส่โค้ดที่นี่
+      <div className='friend-list'>
+        {
+          friends.map ( x=>
+            <div className='friend-list-item'>
+              <Avatar name={x.user}/>
+              {x.displayName}
+            </div>
+            )
+        }
       </div>
     </App>
   )
